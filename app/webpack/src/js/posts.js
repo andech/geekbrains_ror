51ba1bd972ec69a9791b79f5613374b015c7ac0f
andech <7578191@gmail.com>
2018-10-12 18:11:07 +0300

    Fix ajax posts

--- app/webpack/src/js/posts.js
+++ app/webpack/src/js/posts.js
@@ -1,9 +1,13 @@
 $(document).ready(function () {
   $('.load-more').click(function (e) {
     e.preventDefault();
+    var last_id = $('.post-item').last().attr('data-id');
     $.ajax({
         type: "GET",
         url: $(this).attr('href'),
+        data: {
+          id: last_id
+        },
         dataType: "script",
         success: function () {
             $('.load-more').show();
